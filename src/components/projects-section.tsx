6405424526e557bbbf30a503a3d037151a424f46
Aadi Wrld <aadiwrld01@gmail.com>
2025-08-01 11:51:52 +0000

    still not TypeError: 'cubic-bezier(0.6, 0.05, -0.01, 0.9)' is not a vali

--- src/components/projects-section.tsx
+++ src/components/projects-section.tsx
@@ -113,7 +113,7 @@ const cardVariants = {
         scale: 1,
         transition: {
         duration: 0.4,
-        ease: [0.6, 0.05, -0.01, 0.9],
+        ease: [0.6, 0.05, 0.01, 0.9],
         },
     },
 };
