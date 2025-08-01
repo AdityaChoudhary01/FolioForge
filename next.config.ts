bd49e104ac5a552d5c557b5fbcb84f211a66d22f
Aadi Wrld <aadiwrld01@gmail.com>
2025-08-01 11:38:02 +0000

    add these src link for project image , yumride https://adityachportfolio

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,12 @@ const nextConfig: NextConfig = {
         hostname: 'storage.googleapis.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'adityachportfolio3.netlify.app',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
